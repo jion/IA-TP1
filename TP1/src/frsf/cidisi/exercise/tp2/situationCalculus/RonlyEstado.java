9a7bae48ccacab555a85af0bdea1447f6a0e42c1
Manuel S <el.manu@gmail.com>
2013-06-05 10:54:38 -0300

    [TP2] Modificado el estado del agente para calculo situacional

--- TP1/src/frsf/cidisi/exercise/tp2/situationCalculus/RonlyEstado.java
+++ TP1/src/frsf/cidisi/exercise/tp2/situationCalculus/RonlyEstado.java
@@ -13,178 +13,160 @@ import java.util.Hashtable;
 import java.util.List;
 
 public class RonlyEstado extends KnowledgeBase {
-	
+
 	public static final int NORTE = 0;
-	public static final int ESTE  = 1;
-	public static final int SUR   = 2;
+	public static final int ESTE = 1;
+	public static final int SUR = 2;
 	public static final int OESTE = 3;
-	
+
 	// Posibles acciones del Agente
 	public static final int GIR_IZQ = 1;
 	public static final int GIR_DER = 2;
 	public static final int AVANZAR = 3;
 
-	// Variables de estado del Agente
-	private Laberinto		laberinto;		// \
-    private PairInt			posicion;		//  | ¿Dónde Estoy?
-    private int 			orientacion;	// /
-    
-    private boolean 		llave;			//   ¿Qué poseo?
-    
-    private PairInt			posLlave;		// \ ¿Dónde están los objetos importantes?
-    private List<PairInt>	posSalidas;		// /
-    
-    private int				cantDeGiros;    //	 Lleva la cuenta de la cantidad de giros
-
-    private boolean 		ultimoNivel;	//   Me encuentro en el ultimo nivel?
+	// Laberinto actual en la KB.
+	Laberinto laberinto;
 	
-    
-    public RonlyEstado() throws PrologConnectorException {
+	
+	public RonlyEstado() throws PrologConnectorException {
+		// Inicializa la base de conocimientos con el archivo prolog
+		super("kb/ronly.pl");
 
-		//TODO: Replace file name  
-        super("kb/ronly.pl");
+		this.initState();
+	}
 
-        this.initState();
-    }
-
-    @Override
-    public void initState() {
-    	this.posicion   = new PairInt(0,0);
-		this.posLlave   = new PairInt(0,0);
+	@Override
+	public void initState() {
+		// TODO: Que debo definir al iniciar mi KB?
+		// Predicados utilizados?
+		// Posiciones iniciales?
+		
+		
+		/*
+		this.posicion = new PairInt(0, 0);
+		this.posLlave = new PairInt(0, 0);
 		this.posSalidas = new ArrayList<PairInt>();
-		
-    	// En realidad el agente nunca sabra su posicion real actual hasta no
-    	// realizar la primera percepcion.
-    	this.posicion.setPair(0,0);
+
+		// En realidad el agente nunca sabra su posicion real actual hasta no
+		// realizar la primera percepcion.
+		this.posicion.setPair(0, 0);
 		this.orientacion = ESTE;
-		
+
 		this.llave = false;
-		
-		this.posLlave.setPair(0,0);
+
+		this.posLlave.setPair(0, 0);
 		this.posSalidas.clear();
 		this.ultimoNivel = false;
 		this.cantDeGiros = 0;
-    }
-    
-    @Override
-    public ActionFactory getActionFactory() {
-        return RonlyAgentActionFactory.getInstance();
-    }
+		*/
+	}
 
-    @Override
-    public String getSituationPredicate() {
-        return "actualSituation";
-    }
+	// /////////////////////////////////////////////////////////////////////////
+	public void advanceToNextLevel() {
+		int level = this.getLevel();
 
-    @Override
-    public String getBestActionPredicate() {
-        return "bestAction";
-    }
+		// Remove current situation predicate
+		this.removeKnowledge(this.getLevelPredicate() + "(" + level + ")");
 
-    @Override
-    public String getExecutedActionPredicate() {
-        return "action";
-    }
+		// Advance to next situation and add the new situation predicate
+		level = level + 1;
+		this.addKnowledge(this.getLevelPredicate() + "(" + level + ")");
+	}
 
-    @Override
-    public void updateState(Perception p) {
-        this.tell((SituationCalculusPerception) p);
-    }
+	/**
+	 * Devuelve el nivel actual de la Knowledge Base
+	 * 
+	 * @return
+	 */
+	public int getLevel() {
+		String query = this.getLevelPredicate() + "(L)";
 
-    @Override
-    public String toString() {
+		Hashtable[] pos = this.query(query);
+
+		int l = Integer.parseInt(pos[0].get("L").toString());
+
+		return l;
+	}
+
+	public String getLevelPredicate() {
+		return "levelSituation";
+	}
+
+	// /////////////////////////////////////////////////////////////////////////
+
+	@Override
+	public ActionFactory getActionFactory() {
+		return RonlyAgentActionFactory.getInstance();
+	}
+
+	@Override
+	public String getSituationPredicate() {
+		return "actualSituation";
+	}
+
+	@Override
+	public String getBestActionPredicate() {
+		return "bestAction";
+	}
+
+	@Override
+	public String getExecutedActionPredicate() {
+		return "action";
+	}
+
+	@Override
+	public void updateState(Perception p) {
+		this.tell((SituationCalculusPerception) p);
+	}
+
+	@Override
+	public String toString() {
 		StringBuffer str = new StringBuffer();
-		
-		str.append("\n  Posicion: "+ this.getPosicion());
-		str.append("\n  Orientacion: "+ this.getOrientacion());
-		str.append("\n  Tengo llave?: "+ this.isLlave());
+
+		str.append("\n  Posicion: " + this.getPosicion());
+		str.append("\n  Orientacion: " + this.getOrientacion());
+		str.append("\n  Tengo llave?: " + this.tieneLlave());
 		str.append("\n  Laberinto percibido: ");
 		str.append("\n   ");
 		str.append(laberinto.toString());
-		
+
 		return str.toString();
-    }
-
-    // The following methods are agent-specific:
-     public PairInt getPosicion(){
-        return posicion;
-     }
-     
-     public void setPosicion(int x, int y){
-    	 posicion.setFirst(x);
-    	 posicion.setSecond(y);
-     }
-     
-     public int getOrientacion(){
-        return orientacion;
-     }
-     
-     public void setOrientacion(int arg){
-        orientacion = arg;
-     }
-     
-     public boolean getTieneLlave(){
-        return llave;
-     }
-     
-     public void setTieneLlave(boolean tieneLlave){
-        llave = tieneLlave;
-     }
-     
-     public PairInt getPosLlave(){
-        return posLlave;
-     }
-     
-     public void setposLlave(PairInt arg){
-        posLlave = arg;
-     }
-     
-     public List<PairInt> getPosSalidas(){
-        return posSalidas;
-     }
-     
-     public void setPosSalidas(List<PairInt> arg){
-        posSalidas = arg;
-     }
-     
-     public Laberinto getLaberinto(){
-        return laberinto;
-     }
-     
-     public void setLaberinto(Laberinto laberinto){
-        this.laberinto = laberinto;
-     }
-	
-    public boolean isUltimoNivel() {
- 		return ultimoNivel;
- 	}
-
- 	protected void setUltimoNivel(boolean ultimoNivel) {
- 		this.ultimoNivel = ultimoNivel;
- 	}
-
-	protected void setPosicion(PairInt posicion) {
-		this.posicion = posicion;
 	}
 
-	protected void setLlave(boolean llave) {
-		this.llave = llave;
+	// The following methods are agent-specific:
+	private PairInt getPosicion() {
+		String positionQuery = "position(X,Y," + this.getSituation() + ")";
+
+		Hashtable[] result = this.query(positionQuery);
+
+		int x = Integer.parseInt(result[0].get("X").toString());
+		int y = Integer.parseInt(result[0].get("Y").toString());
+
+		return new PairInt(x, y);
 	}
 
-	protected void setPosLlave(PairInt posLlave) {
-		this.posLlave = posLlave;
+	private int getOrientacion() {
+		String positionQuery = "orientation(O," + this.getSituation() + ")";
+
+		Hashtable[] result = this.query(positionQuery);
+
+		return Integer.parseInt(result[0].get("O").toString());
 	}
 
-	public boolean isLlave() {
-		return llave;
+	private boolean tieneLlave() {
+		String keyStateQuery = "sostiene(llave," + this.getSituation() + ")";
+
+		return this.queryHasSolution(keyStateQuery);
 	}
 
+	private PairInt getPosicionLlave() {
+		String positionQuery = "en(llave,X,Y," + this.getSituation() + ")";
 
-	public int getCantDeGiros() {
-		return cantDeGiros;
-	}
+		Hashtable[] result = this.query(positionQuery);
 
-	public void aumentarCantidadDeGiros() {
-		this.cantDeGiros++;
+		int x = Integer.parseInt(result[0].get("X").toString());
+		int y = Integer.parseInt(result[0].get("Y").toString());
+
+		return new PairInt(x, y);
 	}
 }
