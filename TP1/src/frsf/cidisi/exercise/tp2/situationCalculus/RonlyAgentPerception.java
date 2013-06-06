a43369357d7bd876343d194ba057fdf44257355a
Manuel S <el.manu@gmail.com>
2013-06-05 21:11:22 -0300

    [TP2] Modificada la percepcion

--- TP1/src/frsf/cidisi/exercise/tp2/situationCalculus/RonlyAgentPerception.java
+++ TP1/src/frsf/cidisi/exercise/tp2/situationCalculus/RonlyAgentPerception.java
@@ -1,9 +1,6 @@
 package frsf.cidisi.exercise.tp2.situationCalculus;
 
-import frsf.cidisi.exercise.tp1.datastructures.Laberinto;
-import frsf.cidisi.exercise.tp1.datastructures.PairInt;
-import frsf.cidisi.exercise.tp1.search.LaberintosAmbiente;
-import frsf.cidisi.exercise.tp1.search.LaberintosEstado;
+import frsf.cidisi.exercise.datastructures.PairInt;
 import frsf.cidisi.faia.agent.Agent;
 import frsf.cidisi.faia.agent.situationcalculus.SituationCalculusPerception;
 import frsf.cidisi.faia.environment.Environment;
@@ -15,9 +12,22 @@ public class RonlyAgentPerception extends SituationCalculusPerception {
 	 *  - posInicial:  Posicion inicial del agente
 	 *  - ultimoNivel: true si se trata del último nivel (no hay mas niveles)
 	 */
-	private Laberinto	laberinto;
-	private PairInt 	posInicial;
-	private boolean		ultimoNivel;
+	//[x, y, entrada, salida, candado, llave, izquierda, derecha, abajo, arriba, level]
+	
+	private PairInt 	posicion;
+	private boolean		entrada;
+	private boolean		salida;
+	private boolean		candado;
+	private boolean		llave;
+	
+	// Hay pared en alguna orientacion?
+	private boolean		izquierda;
+	private boolean		derecha;
+	private boolean		abajo;
+	private boolean		arriba;
+	
+	// A que nivel pertenece la celda percibida?
+	private int			nivel;
 	
     public RonlyAgentPerception() {
         super();
@@ -25,47 +35,85 @@ public class RonlyAgentPerception extends SituationCalculusPerception {
 		//TODO: Complete Method
     }
 
-    @Override
-    public void initPerception(Agent agent, Environment environmentIn) {
-
-        LaberintosAmbiente environment = (LaberintosAmbiente) environmentIn;
-        LaberintosEstado environmentState =
-                environment.getEnvironmentState();
-
-    }
-
+	@Override
+	public void initPerception(Agent agent, Environment environment) {
+		// TODO Auto-generated method stub
+		
+	}
+	
     @Override
     public String toString() {
-        StringBuffer perceptionString = new StringBuffer("perception([");
+        StringBuffer result = new StringBuffer("perception([");
+      //[x, y, entrada, salida, candado, llave, izquierda, derecha, abajo, arriba, level]
+        // Adjacent cells
+        result.append(this.posicion.getFirst());
+        result.append(",");
+        result.append(this.posicion.getSecond());
+        result.append(",");
+        result.append(this.entrada ? "entrada"	: "nada");
+        result.append(",");
+        result.append(this.salida  ? "salida"	: "nada");
+        result.append(",");
+        result.append(this.candado ? "candado"	: "nada");
+        result.append(",");
+        result.append(this.llave   ? "llave"	: "nada");
+        result.append(",");
+        result.append(this.izquierda ? "izquierda" : "nada");
+        result.append(",");
+        result.append(this.derecha ? "derecha"	: "nada");
+        result.append(",");
+        result.append(this.abajo   ? "abajo"	: "nada");
+        result.append(",");
+        result.append(this.arriba  ? "arriba"	: "nada");
+        result.append(",");
+        result.append(this.nivel);
 
-        //TODO: Complete Method
+        result.append("]");
 
-        return perceptionString.toString();
+        result.append(")");
+
+        return result.toString();
     }
     
     /* Getters & Setters *****************************************************/	
-	public Laberinto getLaberinto(){
-		return laberinto;
+
+	public void setPosicion(PairInt posicion) {
+		this.posicion = posicion;
 	}
 
-	public void setLaberinto(Laberinto percepcionLaberinto){
-		this.laberinto = percepcionLaberinto;
-	}
-	
-	public PairInt getPosInicial() {
-		return posInicial;
+	public void setEntrada(boolean entrada) {
+		this.entrada = entrada;
 	}
 
-	public void setPosInicial(PairInt posInicial) {
-		this.posInicial = posInicial;
+	public void setSalida(boolean salida) {
+		this.salida = salida;
 	}
 
-	public Boolean getUltimoNivel() {
-		return ultimoNivel;
+	public void setCandado(boolean candado) {
+		this.candado = candado;
 	}
-	
-	public void setUltimoNivel(Boolean ultimoNivel) {
-		this.ultimoNivel = ultimoNivel;
+
+	public void setLlave(boolean llave) {
+		this.llave = llave;
 	}
-	
+
+	public void setIzquierda(boolean izquierda) {
+		this.izquierda = izquierda;
+	}
+
+	public void setDerecha(boolean derecha) {
+		this.derecha = derecha;
+	}
+
+	public void setAbajo(boolean abajo) {
+		this.abajo = abajo;
+	}
+
+	public void setArriba(boolean arriba) {
+		this.arriba = arriba;
+	}
+
+	public void setNivel(int nivel) {
+		this.nivel = nivel;
+	}
 }
